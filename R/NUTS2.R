79b09254511171c73b9e296b2ff1d25012624a44
fabiosepulveda <fasepul@outlook.com>
2023-05-26 10:20:01 -0500

    Actualizar data

---
+++ R/NUTS2.R
@@ -0,0 +1,23 @@
+#' European Union
+#'
+#' A subset of data with regions or NUTS-2 from European Union.
+#'
+#' @format ## `NUTS2`
+#' A data frame with 233 rows (regions) and 11 columns (variables):
+#' \describe{
+#'   \item{region}{Region number}
+#'   \item{id}{Region codes}
+#'   \item{name}{Region name}
+#'   \item{munemp}{Male unemployment. Unemployment rate % from 20 to 64 years (male).}
+#'   \item{funemp}{Female unemployment. Unemployment rate % from 20 to 64 years (female).}
+#'   \item{migrant}{Crude rate of net migration plus statistical adjustment.}
+#'   \item{assault}{Number of deaths by assault and homicide divided by population and then multiplied by 100,000 (crude death rate).}
+#'   \item{tertiary}{Individuals aged 25-64 who successfully completed tertiary education (levels 5-8 ISCED 2011) over the population with the same age (In %).}
+#'   \item{hrht}{Human resources in technology. Persons employed in science and technology as percentage of active population.}
+#'   \item{rcd}{Registered community designs. Number of registered community designs per billion GDP purchasing power standards.}
+#'   \item{gdp_gini}{Regional gross domestic product (GDP) purchasing power standard per inhabitant adjusted by the country Gini index of disposable household income.}
+#'
+#'
+#' }
+#' @source <https://ec.europa.eu/eurostat>
+"NUTS2"
